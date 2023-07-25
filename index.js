e95b47fd90e529260898893378e3e42075710aa9
brandonkhoza <8brandonkhoza@gmail.com>
2023-07-25 15:58:19 +0200

    Added Nested States and Immer functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //create a store and initialze the state
+const { produce } = require('immer');
 const redux = require('redux');
 const createStore = redux.createStore;
 
@@ -7,7 +8,8 @@ const bindActionsCreators = redux.bindActionCreators;
 
 //bind/combine Reducers creators
 const combineReducers = redux.combineReducers;
-
+//immer for nested states
+const produceImmer = require('immer').produce
 
 //====================================================================
 //create an action
@@ -16,6 +18,10 @@ const CAKE_RESTOCK = 'CAKE_RESTOCK';
 
 const ICE_ORDERED = 'ICE_ORDERED';
 const ICE_RESTOCK = 'ICE_RESTOCK';
+
+const UPDATE_CITY = 'UPDATE_CITY';
+const UPDATE_STREET = 'UPDATE_STREET';
+const UPDATE_ZIP = 'UPDATE_ZIP';
 
 function orderCake(qty = 0) {
 
@@ -43,16 +49,44 @@ function restockIce(qty = 0) {
         qty
     }
 }
+function updateStreet(street = '') {
+    return {
+        type: UPDATE_STREET,
+        street
+    }
+}
+function updateZip(zip = '') {
+    return {
+        type: UPDATE_ZIP,
+        zip
+    }
+}
+function updateCity(city = '') {
+    return {
+        type: UPDATE_CITY,
+        city
+    }
+}
 //====================================================================
 
 //====================================================================
 //initializing states
+
 const initialStateIce = {
 
     numOfIce: 10
 }
 const initialStateCake = {
     numOfCake: 10
+}
+const initialStoreDetails = {
+    name: 'Tee Cube',
+    address: {
+        city: 'Pretoria',
+        street: 'Lilian',
+        zip: 1234
+    }
+
 }
 //create a Reducer
 const reducerCake = (state = initialStateCake, action) => {
@@ -82,13 +116,29 @@ const reducerIce = (state = initialStateIce, action) => {
         }
     }
 }
+const reducerStoreDetails = (state = initialStoreDetails, action) => {
+    switch (action.type) {
+        case UPDATE_STREET: return {
+            ...state, address: { ...state.address, street: action.street }
+        }
+        case UPDATE_ZIP: return {
+            ...state, address: { ...state.address, zip: action.zip }
+        }
+        case UPDATE_CITY: return produce(state, (prodState) => {
+            prodState.address.city = action.city
+        })
+        default:
+            return state;
+    }
+}
 //====================================================================
 
 //====================================================================
 //combine reducers
 const rootReducer = combineReducers({
     cake: reducerCake,
-    ice: reducerIce
+    ice: reducerIce,
+    storeInfo: reducerStoreDetails
 })
 //initialize the store
 const store = createStore(rootReducer);
@@ -102,16 +152,23 @@ const unsubcribe = store.subscribe(() => {
 });
 //dispatching actions
 // store.dispatch(orderCake());
-// store.dispatch(orderCake(2));
-// store.dispatch(restockCake(10));
-// store.dispatch(orderCake(1));
+// store.dispatch(orderCake(2)); 
 
 // action binder takes in objects and the store.dispatch
-const actionCreators = bindActionsCreators({ orderCake, restockCake, orderIce, restockIce, }, store.dispatch);
+const actionCreators = bindActionsCreators({
+    orderCake, restockCake,
+    orderIce, restockIce,
+    updateStreet, updateZip, updateCity
+}, store.dispatch);
+
 actionCreators.orderCake(2);
 actionCreators.restockCake(10);
 actionCreators.orderCake(1);
 actionCreators.restockIce(5);
+
+actionCreators.updateStreet('Alex');
+actionCreators.updateZip(4321);
+actionCreators.updateCity('Johannesburg');
 //====================================================================
 
 unsubcribe();
